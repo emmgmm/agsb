07cd4896d5bd793c867dbe2e6087879b8b010b53
emmgmm <71999323+emmgmm@users.noreply.github.com>
2025-06-06 12:40:12 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -34,8 +34,8 @@ CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的�
 USER_NAME = "emme"         # 用户名
 UUID = "d5c5e27c-1d01-49a2-b173-9e49b2f82b1c"                     # UUID，留空则自动生成
 PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+DOMAIN = "agsb.sunwax.dpdns.org"                   # 域名，留空则自动获取
+CF_TOKEN = "eyJhIjoiM2M1OWQxODhlYmNjMTYxNDA3ODgyMWI5YTE1NDA5NTAiLCJ0IjoiMmUyOTQ3ZTItN2NkOC00YWZmLTg5NGEtMDI2Njc0MGY1OGY0IiwicyI6Ik9USTNOV1pqTkRJdFlURTVNeTAwTXpNekxUbGlOalV0T1RFNFptWmpaRGxqWWpZdyJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
